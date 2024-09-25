22f83ab3517e5812f17a53729c3a368b39b2b339
chijan0427 <wlsnd0427@gmail.com>
2024-09-25 21:25:14 +0900

    Add files via upload

---
+++ new folder12/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("어서오세요. 시작의 마을에");
+            Console.WriteLine("던전에 입장하시기 전에 무엇을 하실건가요?");
+            Console.WriteLine(" ");
+            Console.WriteLine("1. 상태 보기");
+            Console.WriteLine("2. 인벤토리");
+            Console.WriteLine("3. 상태창");
+            Console.WriteLine(" ");
+            Console.WriteLine("원하시는 행동을 입력해주세요.");
+            
+        }
+    }
+}
